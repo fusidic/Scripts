59e38ac512bab7db0fc7016f83a660096e203b74
fusidic <arithbar@gmail.com>
2020-04-08 11:33:51 +0800

    fix: exit node server on Command hexo e

--- Alfred/hexo.sh
+++ Alfred/hexo.sh
@@ -7,6 +7,11 @@ if [ "$query" = "s" ];then
     open /Applications/Safari.app http://localhost:4000
 fi
 
+# Exit node server
+if [ "$query" = "e" ];then
+    lsof -P | grep ':4000' | awk '{print $2}' | xargs kill -9
+fi
+
 if [ "$query" = "b" ] || [ "$query" = "d" ];then
     # 检查/Users/fusidic/Documents/notes文件夹的新文件，并加入hugo中
     path='/Users/fusidic/Documents/notes/'
@@ -28,7 +33,9 @@ if [ "$query" = "b" ] || [ "$query" = "d" ];then
             open -a /Applications/Typora.app /Users/fusidic/Documents/work/hexo/fusidic/source/_posts/${filename}
         fi
     done
+	open -a /Applications/iTerm.app /Users/fusidic/Documents/work/hexo/fusidic
 fi
+
 
 if [ "$query" = "d" ];then
     cd /Users/fusidic/Documents/work/hexo/fusidic
