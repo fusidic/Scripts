cb6e0fa488f3439619bb5a673856c424bc69e81c
fusidic <arithbar@gmail.com>
2020-08-25 15:41:17 +0800

    fix: yum-config-manager

--- CentOS-sys-init.sh
+++ CentOS-sys-init.sh
@@ -79,7 +79,7 @@ install_docker()
 {
     sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
     sudo yum install -y yum-utils
-    sudo yum-config-namager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     sudo yum install docker-ce docker-ce-cli containerd.io
     sudo systemctl start docker
 }
