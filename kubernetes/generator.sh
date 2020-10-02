13f6a1094a652ea4de63378f3889241c5ca87886
fusidic <arithbar@gmail.com>
2020-10-03 01:03:07 +0800

    holiday push

--- kubernetes/generator.sh
+++ kubernetes/generator.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-
-
-FILE="Compute1.txt"
+FILE="testcase.txt"
 NAME="eagle"
 SCHEDULER_NAME="eagle-scheduler"
 
 # 计算总行数
 LINES=$(wc -l $FILE | awk '{print $1}')
 
+if [ ! -f "./yaml" ]
+then
+    mkdir yaml
+fi
+
 # 遍历
 for ((i=1; i<=$LINES; i ++))
 do
-    CPU_USAGE=$(sed -n ""$i"p" $FILE | awk '{print $3}')
-    MEM_USAGE=$(sed -n ""$i"p" $FILE | awk '{print $4}')
+    CPU_USAGE=$(sed -n ""$i"p" $FILE | awk '{print $3}')"m"
+    MEM_USAGE=$(sed -n ""$i"p" $FILE | awk '{print $4}')"Mi"
     TARGET_FILE="./yaml/eagle_"$i".yaml"
-    echo "cat ./base.yaml > $TARGET_FILE"
+    cat ./base.yaml > $TARGET_FILE
 
     POD_NAME="$NAME"-"$i"
 
-    # 写入目标值
-    echo "yq w -i "$TARGET_FILE" 'spec.schedulerName' $POD_NAME"
-    echo "yq w -i "$TARGET_FILE" 'metadata.name' $SCHEDULER_NAME"
-    echo "yq w -i "$TARGET_FILE" 'spec.containers[0].resources.limits.cpu' $CPU_USAGE"
-    echo "yq w -i "$TARGET_FILE" 'spec.containers[0].resources.limits.memory' $MEM_USAGE"
-    echo "yq w -i "$TARGET_FILE" 'spec.containers[0].resources.requests.cpu' $CPU_USAGE"
-    echo "yq w -i "$TARGET_FILE" 'spec.containers[0].resources.requests.memory' $MEM_USAGE"
+    # 写入目标值到 ./yaml 路径下
+    yq w -i "$TARGET_FILE" 'spec.schedulerName' $SCHEDULER_NAME
+    yq w -i "$TARGET_FILE" 'metadata.name' $POD_NAME
+    yq w -i "$TARGET_FILE" 'spec.containers[0].resources.limits.cpu' $CPU_USAGE
+    yq w -i "$TARGET_FILE" 'spec.containers[0].resources.limits.memory' $MEM_USAGE
+    yq w -i "$TARGET_FILE" 'spec.containers[0].resources.requests.cpu' $CPU_USAGE
+    yq w -i "$TARGET_FILE" 'spec.containers[0].resources.requests.memory' $MEM_USAGE
+
 done
 
---
+++ kubernetes/merge-YAML.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+path='/Volumes/nuaa/yaml'
+files=$(ls $path)
+echo $files
+
+# touch testcase.yaml
+
+# for filename in $files
+# do
+#     cat "${path}/${filename}" >> testcase.yaml
+#     echo "---" >> testcase.yaml
+# done
+    
