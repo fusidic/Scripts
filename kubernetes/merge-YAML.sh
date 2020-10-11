b67b8a7a530eef99231c4543e0a7f11418dbb58a
fusidic <arithbar@gmail.com>
2020-10-11 09:51:06 +0800

    daily push

--- kubernetes/merge-YAML.sh
+++ kubernetes/merge-YAML.sh
@@ -11,4 +11,9 @@ echo $files
 #     cat "${path}/${filename}" >> testcase.yaml
 #     echo "---" >> testcase.yaml
 # done
+
+# 以第一列的数值为准进行去重
+sort -k 1,1 -u Memory_bias.txt > Memory_slim.txt
     
+# 分别求第三列与第四列的累加和
+cat testcase.txt| awk 'BEGIN{memSum=0;cpuSum=0}{memSum+=$4}{cpuSum+=$3}END{print cpuSum"\t"memSum"\t"cpuSum/memSum}'
